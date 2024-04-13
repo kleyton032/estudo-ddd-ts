227d1bb89dca89fb7cbd50750a4519509dc3987d
Kleyton João <kleyton.joao@gmail.com>
2024-04-13 07:05:34 -0300

    mapping class properties

--- src/domain/use-cases/answer-question.ts
+++ src/domain/use-cases/answer-question.ts
@@ -1,10 +1,13 @@
+import { UniqueEntityID } from "../../core/entities/unique-entity-id"
 import { Answer } from "../entities/answer"
 import { AnswerRepository } from "../repositories/answer-respository"
 
 interface AnswerQuestionUseCaseRequest {
-    instructorId: string
-    questionId: string
-    content: string
+    instructorId: UniqueEntityID
+    questionId: UniqueEntityID
+    content: string,
+    createdAt: Date
+    updatedAt?: Date
 }
 
 
@@ -12,11 +15,12 @@ export class AnswerQuestionUseCase {
 
     constructor(private answerRepository: AnswerRepository){}
 
-    async execute({ instructorId, questionId, content }: AnswerQuestionUseCaseRequest) {
+    async execute({ instructorId, questionId, content, createdAt }: AnswerQuestionUseCaseRequest) {
         const answer = new Answer({
             content,
             authorId: instructorId,
             questionId,
+            createdAt
         })
 
         await this.answerRepository.create(answer)
