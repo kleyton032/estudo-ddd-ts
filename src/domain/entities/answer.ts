b2e86a0b9fc21425882a61bfc7408a9679e0f55e
Kleyton João <kleyton.joao@gmail.com>
2026-02-11 21:35:20 -0300

    abstracting the creation of entities

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -1,6 +1,7 @@
 import { randomUUID } from "node:crypto"
 import { Entity } from "../../core/entities/entity"
 import { UniqueEntityID } from "../../core/entities/unique-entity-id"
+import { Optional } from "../../core/types/optional"
 
 interface AnswerProps {
     authorId: UniqueEntityID
@@ -8,13 +9,21 @@ interface AnswerProps {
     content: string
     createdAt: Date
     updatedAt?: Date
-   
+
 }
 
-export class Answer extends Entity<AnswerProps>{
+export class Answer extends Entity<AnswerProps> {
 
     get content() {
         return this.props.content
     }
-   
+
+    static create(props: Optional<AnswerProps, 'createdAt'>, id?: UniqueEntityID) {
+        const answer = new Answer({
+            ...props,
+            createdAt: new Date(),
+        }, id)
+        return answer
+    }
+
 }
--- src/domain/entities/instructor.ts
+++ src/domain/entities/instructor.ts
@@ -1,5 +1,6 @@
 import { randomUUID } from "node:crypto"
 import { Entity } from "../../core/entities/entity"
+import { UniqueEntityID } from "../../core/entities/unique-entity-id"
 
 
 interface InstructorProps {
@@ -7,4 +8,12 @@ interface InstructorProps {
 }
 
 
-export class Instructor extends Entity<InstructorProps>{}
+export class Instructor extends Entity<InstructorProps> {
+
+    static create(props: InstructorProps, id?: UniqueEntityID) {
+        const instructor = new Instructor({
+            ...props
+        }, id)
+        return instructor
+    }
+}
